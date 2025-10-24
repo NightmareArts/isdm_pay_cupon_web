ec98ad21cafd69c1ea6ab31c22904ec574ccede5
Emiliano Daniel Guerrero <guerrero.emiliano@outlook.com>
2025-10-24 16:04:16 -0300

    update user/admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("Rol", {"fields": ("role",)}),)
+    list_display = ("username", "email", "role", "is_staff", "is_superuser")
+    list_filter = ("role", "is_staff", "is_superuser", "is_active")
